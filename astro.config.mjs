d0250344c6838f05aed1be22e0048a56c2fb8080
LGsus113 <weikgoggowb@gmail.com>
2025-02-20 23:17:51 -0600

    arreglo de ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,13 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import node from "@astrojs/node";
+import netlify from "@astrojs/netlify/functions";
 import { resolve } from "path";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: node({
-    mode: "standalone",
-  }),
+  adapter: netlify({}),
   integrations: [tailwind()],
   vite: {
     resolve: {
