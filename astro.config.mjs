bb8371618a19d788fcd090c5cec3059873588cd0
LGsus113 <weikgoggowb@gmail.com>
2025-02-20 23:01:35 -0600

    ssr activado y manejo de filtro de poemas hecho

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import node from "@astrojs/node";
 import { resolve } from "path";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
+  adapter: node({
+    mode: "standalone",
+  }),
   integrations: [tailwind()],
   vite: {
     resolve: {
